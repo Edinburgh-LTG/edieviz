84ecb9ab577c54e6c997bcd9f1a5421a0769e145
Andreas Grivas <andreasgrv@gmail.com>
2021-04-28 20:26:37 +0100

    Add script to download EdIE models

---
+++ models/download_models.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+wget -q --show-progress http://jekyll.inf.ed.ac.uk/edieviz/static/models/EdIE-LOUHI.zip
+if [ $? -eq 0 ]; then
+	echo '## Downloaded models successfully...'
+	echo '## Unzipping...'
+	unzip EdIE-LOUHI.zip
+	if [ $? -eq 0 ]; then
+		echo '## Tidying up directory...'
+		rm EdIE-LOUHI.zip
+		mv EdIE-LOUHI/LOUHI-EdIE-N-v1 .
+		mv EdIE-LOUHI/LOUHI-ncbi-bert .
+		rmdir EdIE-LOUHI
+	fi
+else
+	echo 'Failed downloading models, exiting...'
+	exit 1
+fi
+
+# Adapt paths in downloaded blueprints to be the global paths
